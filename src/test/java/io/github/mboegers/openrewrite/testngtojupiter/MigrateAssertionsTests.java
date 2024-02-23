5f92af028a0acb65320b3fbef97ce0b949034af0
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2024-02-23 19:01:25 +0100

    Enhance tests for Assert.assertEquals

--- src/test/java/io/github/mboegers/openrewrite/testngtojupiter/MigrateAssertionsTests.java
+++ src/test/java/io/github/mboegers/openrewrite/testngtojupiter/MigrateAssertionsTests.java
@@ -11,9 +11,12 @@ package io.github.mboegers.openrewrite.testngtojupiter;
 
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.openrewrite.java.JavaParser;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
+import org.testng.Assert;
 
 import static org.openrewrite.java.Assertions.java;
 
@@ -28,66 +31,96 @@ class MigrateAssertionsTests implements RewriteTest {
 
     @Nested
     class MigrateAssertEquals {
-        @Nested
-        class WithFailMessage {
-            @Test
-            void migrateEqualsInt() {
-                //language=java
-                rewriteRun(java("""
+        @ParameterizedTest
+        @ValueSource(strings = {
+          "boolean", "boolean[]",
+          "byte", "byte[]",
+          "char", "char[]",
+          "double", "double[]",
+          "float", "float[]",
+          "int", "int[]",
+          "long", "long[]",
+          "short", "short[]",
+          "java.lang.Boolean", "java.lang.Boolean[]",
+          "java.lang.Character", "java.lang.Character[]",
+          "java.lang.Double", "java.lang.Double[]",
+          "java.lang.Float", "java.lang.Float[]",
+          "java.lang.Integer", "java.lang.Integer[]",
+          "java.lang.String", "java.lang.String[]",
+          "java.util.Map<?,?>", "java.util.Set<?>"
+        })
+        void withErrorMessage(String type) {
+            //language=java
+            rewriteRun(java("""
                     import org.testng.Assert;
                          
                     class MyTest {
-                        void testInt() {
-                            int actual = 1;
-                            int expected = 2;
+                        void testMethod() {
+                            %s actual;
+                            %s expected;
                           
                             Assert.assertEquals(actual, expected, "Test failed badly");
                         }
                     }
-                    """, """
+                    """.formatted(type, type), """
                     import org.junit.jupiter.api.Assertions;
                  
                     class MyTest {
-                        void testInt() {
-                            int actual = 1;
-                            int expected = 2;
+                        void testMethod() {
+                            %s actual;
+                            %s expected;
                                          
                             Assertions.assertEquals(expected, actual, "Test failed badly");
                         }
                     }
-                    """));
-            }
+                    """.formatted(type, type)));
         }
 
-        @Nested
-        class WithoutFailMessage {
-            @Test
-            void migrateEqualsInt() {
-                //language=java
-                rewriteRun(java("""
+        @ParameterizedTest
+        @ValueSource(strings = {
+          "boolean", "boolean[]",
+          "byte", "byte[]",
+          "char", "char[]",
+          "double", "double[]",
+          "float", "float[]",
+          "int", "int[]",
+          "long", "long[]",
+          "short", "short[]",
+          "java.lang.Boolean", "java.lang.Boolean[]",
+          "java.lang.Character", "java.lang.Character[]",
+          "java.lang.Double", "java.lang.Double[]",
+          "java.lang.Float", "java.lang.Float[]",
+          "java.lang.Integer", "java.lang.Integer[]",
+          "java.lang.String", "java.lang.String[]",
+          "java.util.Map<?,?>", "java.util.Set<?>"
+        })
+        void withoutErrorMessage(String type) {
+            //language=java
+            rewriteRun(java("""
                     import org.testng.Assert;
-                    
+                         
                     class MyTest {
-                        void testInt() {
-                            int actual = 1;
-                            int expected = 2;
-                            
+                        void testMethod() {
+                            %s actual;
+                            %s expected;
+                          
                             Assert.assertEquals(actual, expected);
                         }
                     }
-                    """, """
+                    """.formatted(type, type), """
                     import org.junit.jupiter.api.Assertions;
                  
                     class MyTest {
-                        void testInt() {
-                            int actual = 1;
-                            int expected = 2;
+                        void testMethod() {
+                            %s actual;
+                            %s expected;
                                          
                             Assertions.assertEquals(expected, actual);
                         }
                     }
-                    """));
-            }
+                    """.formatted(type, type)));
         }
     }
+
+
 }
