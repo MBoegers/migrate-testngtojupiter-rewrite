97b8d35329808cc4a6d3884d5e817c18856a1eb9
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2024-02-23 14:17:04 +0100

    migrate to java 11 and add missing dependencies to Juniper and TestNG

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:latest.release"))
 
     implementation("org.openrewrite:rewrite-java")
-    runtimeOnly("org.openrewrite:rewrite-java-17")
+    runtimeOnly("org.openrewrite:rewrite-java-11")
 
     // Refaster style recipes need the rewrite-templating annotation processor and dependency for generated recipes
     // https://github.com/openrewrite/rewrite-templating/releases
@@ -35,8 +35,14 @@ dependencies {
     // Need to have a slf4j binding to see any output enabled from the parser.
     runtimeOnly("ch.qos.logback:logback-classic:1.2.+")
 
+    // to be able to use Juniper API and TestNG classes in recipes
+    implementation("org.junit.jupiter:junit-jupiter-api:latest.release")
+    implementation("org.testng:testng:latest.release")
+
     // Our recipe converts Guava's `Lists` type
     testRuntimeOnly("com.google.guava:guava:latest.release")
+    testRuntimeOnly("org.testng:testng:latest.release")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-api:latest.release")
 
     // Contains the OpenRewriteBestPractices recipe, which you can apply to your recipes
     rewrite("org.openrewrite.recipe:rewrite-recommendations:latest.release")
@@ -57,4 +63,8 @@ publishing {
           url = uri("https://us-west1-maven.pkg.dev/moderne-dev/moderne-recipe")
       }
   }
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    options.release.set(11)
 }
